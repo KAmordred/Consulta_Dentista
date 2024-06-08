9fde73e89d838fedf6d5324f67c81fb74be62222
Alan Barrera <131992781+KAmordred@users.noreply.github.com>
2024-06-07 21:36:34 -0400

    alancito bonito

---
+++ core/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contactanos/', views.contactanos, name='contactanos'),
+    path('nosotros/', views.nosotros, name='nosotros'),
+    path('especialidades/', views.especialidades, name='especialidades'),
+    path('insumos/', views.insumos, name='insumos'),
+    path('VentaEquiposRX/', views.venta_equipos_rx, name='AVentaEquiposRX'),
+    path('AVentaSillas/', views.venta_sillas, name='AVentaSillas'),
+    path('AVentaSillonesFull/', views.venta_sillones_full, name='AVentaSillonesFull'),
+    path('AVentaSillonesViejos/', views.venta_sillones_viejos, name='AVentaSillonesViejos'),
+    path('BVentaImplantes/', views.venta_implantes, name='BVentaImplantes'),
+    path('BVentaInstrumentos/', views.venta_instrumentos, name='BVentaInstrumentos'),
+    path('BVentaKitCompleto/', views.venta_kit_completo, name='BVentaKitCompleto'),
+    path('BVentaResinas/', views.venta_resinas, name='BVentaResinas'),
+    path('sillones/', views.sillones, name='sillones'),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
+def index(request):
+    return render(request, 'index.html')
+
+def contactanos(request):
+    return render(request, 'contactanos.html')
+
+def nosotros(request):
+    return render(request, 'nosotros.html')
+
+def especialidades(request):
+    return render(request, 'especialidades.html')
+
+def insumos(request):
+    return render(request, 'insumos.html')
+
+def sillones(request):
+    return render(request, 'sillones.html')
+
+def venta_equipos_rx(request):
+    return render(request, 'AVentaEquiposRX.html')
+
+def venta_sillas(request):
+    return render(request, 'AVentaSillas.html')
+
+def venta_sillones_full(request):
+    return render(request, 'AVentaSillonesFull.html')
+
+def venta_sillones_viejos(request):
+    return render(request, 'AVentaSillonesViejos.html')
+
+def venta_implantes(request):
+    return render(request, 'BVentaImplantes.html')
+
+def venta_instrumentos(request):
+    return render(request, 'BVentaInstrumentos.html')
+
+def venta_kit_completo(request):
+    return render(request, 'BVentaKitCompleto.html')
+
+def venta_resinas(request):
+    return render(request, 'BVentaResinas.html')
 # Create your views here.
