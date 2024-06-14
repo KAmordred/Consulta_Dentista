b7aa7a17cb9b53a342276d2b564e1664824f7c4e
Alan Barrera <alangabriel49@gmail.com>
2024-06-14 17:03:13 -0400

    base de datos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    cantidad_disponible = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class Pedido(models.Model):
+    fecha_pedido = models.DateTimeField(auto_now_add=True)
+    productos = models.ManyToManyField(Producto, through='DetallePedido')
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Pedido #{self.id} - {self.fecha_pedido}"
+
+
+class DetallePedido(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField()
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Detalle de Pedido #{self.pedido.id} - {self.producto.nombre}"
 # Create your models here.
